57bf0989e95146e6ad67205c9ea31fee252912bb
Ana Jovanovic <anaxjovanovicx@gmail.com>
2019-10-14 15:19:03 +0200

    Added docker support (resolves issue #3).

---
+++ samples/Geta.ScheduledParameterJob.Sample/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using Geta.ScheduledParameterJob.Sample.Models.ViewModels;
+
+namespace Geta.ScheduledParameterJob.Sample.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
